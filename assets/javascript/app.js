2c22f2d0b07721a135f57dc6467b0423c489bbc0
Gigi Tannous <gtannous16@gmail.com>
2019-03-27 17:52:40 -0400

    added some audio for right/wrong questions and added the questions in javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+    
+    var questions = [{
+       question:"Which Friend hates thanksgiving?",
+       answerList:["Ross","Chandler","Phoebe","Joey"],
+       answer:1,
+       image: "assets/images/thanksgiving.gif"
+    },{
+        question: "What Name did Ross say at the altar?",
+        answerList: ["Emily","Nicole","Emma","Rachel"],
+        answer:3,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"What was the name of Chandler's on-again off-again girlfriend throughout the series?",
+        answerList:["Julie","Susie","Mary-Angela","Janice"],
+        answer:3,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"Who officiated Monica and Chandlers Wedding?",
+        answerList:["Phoebe", "Ross","Joey","A Greek Orthodox Priest"],
+        answer:2,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"What type of animal was Ross's pet by the name of Marcel?",
+        answerList:["Dog","Cat","Monkey","Duck"],
+        answer:2,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"What is Phoebe's twin sisters name?",
+        answerList:["Ursula","Regina","Jenny","Carol"],
+        answer:0,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"Which Friend was obese in high school?",
+        answerList:["Joey","Monica","Rachel","Chandler"],
+        answer:1,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"What type of animals were Joey and Chandler's two pets?",
+        answerList:["Dog & Cat","Chick & Duck","Monkey & Cat","Dog & Duck"],
+        answer:1,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"What' the name of Rachel and Monica's annoying neighbor?",
+        answerList:["Mr. Heckles","Mr. Davis","Mr. Flanigan","Mr. Phalanges"],
+        answer:0,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"Who lives across the street from Monica & Rachel?",
+        answerList:["Ugly Guy","Fat Guy","Ugly Naked Girl","Ugly Naked Guy"],
+        answer:3,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"How many times has Ross been married?",
+        answerList:["5 times","3 times","7 times","4 times"],
+        answer:1,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"Who did Rachel leave at the altar?",
+        answerList:["Ross","Barry","Paolo","Chandler"],
+        answer:1,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"Who does Monica marry?",
+        answerList:["Joey","Richard","Chandler","Funny Bobby"],
+        answer:2,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"Who does Joey play on Days of our Lives",
+        answerList:["Lucky Spencer","Dr. Drake Ramoray","Matt LeBlanc","Bo Brady"],
+        answer:1,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"Whats the name of Phoebe's altar ego",
+        answerList:["princess consuela banana hammock","Frances","Regina Falange","Rachel Greene"],
+        answer:2,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"Who has a 'nubbin?",
+        answerList:["Joey","Ugly Naked Guy","Richard","Chandler"],
+        answer:3,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"What is the name of Joey's Chair?",
+        answerList:["Susie","Jenny","Rosita","Stevie"],
+        answer:2,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"When Ross was a kid whe created a comic book, What was the name of the main Character?",
+        answerList:["DinoMan","Science Boy","Super Paleontologist","Bob"],
+        answer:1,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        question:"Who always says, 'WE WERE ON A BREAK!'",
+        answerList:["Chandler","Fun Bobby","Ross","The Mattress King"],
+        answer:2,
+        image:"assets/images/thanksgiving.gif"
+    },{
+        quetion:"What is Janice's famous catchphrase?",
+        answerList:["Janice's day of fun!","OH.MY.GOD.","Mr. Chandler Bing","BingALing"],
+        answer:1,
+        image:"assets/images/thanksgiving.gif"
+    }]
+});
